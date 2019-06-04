c244631cf56b949d441c4eaf09f4d821618d0954
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-04 14:33:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1,rightDiceNumber = 2;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,13 +30,13 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: () {},
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {},
             ),
           ),
